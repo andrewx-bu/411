d214fd91b70dcfb437e76c817b96305e40695616
andrewx-bu <andrewx@bu.edu>
2024-10-07 17:21:44 -0400

    moved some variables around

--- HW_3/wildlife_tracker/migration_tracking/migration.py
+++ HW_3/wildlife_tracker/migration_tracking/migration.py
@@ -1,7 +1,10 @@
 from migration_path import MigrationPath
 
 class Migration:
-    def __init__(self, migration_id: int, migration_path: MigrationPath, status: str = "Scheduled"):
+    def __init__(self, migration_id: int, migration_path: MigrationPath, current_location: str, start_date: str, current_date: str, status: str = "Scheduled"):
         self.migration_id = migration_id
         self.migration_path = migration_path
+        self.current_location = current_location
         self.status = status
+        self.start_date = start_date
+        self.current_date = current_date
